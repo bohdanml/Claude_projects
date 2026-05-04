1717935efa7393b877462e9a4daa1b3ec89f5d7d
bohdanml <bmihajliv558@gmail.com>
2026-05-04 15:58:45 +0300

    chore: add comment for year input and fix missing newline at EOF

--- Skills_1/main.py
+++ Skills_1/main.py
@@ -1,9 +1,10 @@
 import calendar
 
 print ('Вітаю в кадендарі!\n')
-
+#input year
 year = int(input('Введіть рік: '))
 
 month = int(input('Введіть номер місяця: '))
 
 print(calendar.month(year,month))
+
